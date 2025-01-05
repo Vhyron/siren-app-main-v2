ba1bdcfaf9b4e871737d212feb5d86d6b0bc4bc7
Rai M. Reyes Jr. <2r.reiyhes@gmail.com>
2025-01-05 22:02:01 +0800

    fix admin reports details

--- app/admin/emergency_report/report_detail.tsx
+++ app/admin/emergency_report/report_detail.tsx
@@ -1,15 +1,16 @@
-import { View, Text, Image, ScrollView } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import AdminStyledContainer from '@/components/admin/AdminStyledContainer';
-import AdminHeader from '@/components/admin/AdminHeader';
-import { useRouter } from 'expo-router';
-import { get, ref } from 'firebase/database';
-import { db } from '@/firebaseConfig';
-import MapView, { Marker } from 'react-native-maps';
-import { useLocalSearchParams } from 'expo-router';
-import { formatDate } from '@/constants/Date';
-import { ScaledSheet } from 'react-native-size-matters';
-import Loading from '@/components/app/Loading';
+import { View, Text, Image, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import AdminStyledContainer from "@/components/admin/AdminStyledContainer";
+import AdminHeader from "@/components/admin/AdminHeader";
+import { useRouter } from "expo-router";
+import { get, ref } from "firebase/database";
+import { db } from "@/firebaseConfig";
+import MapView, { Marker } from "react-native-maps";
+import { useLocalSearchParams } from "expo-router";
+import { formatDate } from "@/constants/Date";
+import { ScaledSheet } from "react-native-size-matters";
+import Loading from "@/components/app/Loading";
+import ImageModal from "react-native-image-modal";
 
 export default function ReportDetail() {
   const router = useRouter();
@@ -27,13 +28,31 @@ export default function ReportDetail() {
 
           if (reportSnapshot.exists()) {
             console.log(reportSnapshot.val());
-            setReport(reportSnapshot.val());
+            const reportData = reportSnapshot.val();
+            if (reportData.senderId) {
+              const userRef = ref(db, `users/${reportData.senderId}`);
+
+              const userSnapshot = await get(userRef);
+
+              if (userSnapshot.exists()) {
+                const userData = userSnapshot.val();
+                reportData.reporterName =
+                  userData.firstname + " " + userData.lastname ||
+                  "Unknown Reporter"; // Assign reporter's name
+              } else {
+                reportData.reporterName = "Unknown Reporter";
+              }
+            } else {
+              reportData.reporterName = "Unknown Reporter";
+            }
+
+            setReport(reportData);
           } else {
-            console.error('Report not found');
+            console.error("Report not found");
           }
         }
       } catch (error) {
-        console.error('Error fetching report details:', error);
+        console.error("Error fetching report details:", error);
       } finally {
         setLoading(false);
       }
@@ -41,6 +60,11 @@ export default function ReportDetail() {
 
     fetchReportDetails();
   }, []);
+
+  const formatNotificationTime = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  };
 
   if (loading) return <Loading />;
 
@@ -53,17 +77,21 @@ export default function ReportDetail() {
       <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
         <View style={styles.reportsContainer}>
           <View style={styles.reportDesc}>
-            <Text style={styles.descName}>{report?.reporterName || 'Unknown Reporter'}</Text>
-            <Text style={styles.descMessage}>{report?.details || ''}</Text>
+            <Text style={styles.descName}>
+              <Text style={styles.descName}>
+                {report?.reporterName || "Unknown Reporter"}
+              </Text>
+            </Text>
+            <Text style={styles.descMessage}>{report?.details || ""}</Text>
             <Text style={styles.descTime}>
-              {report?.timestamp ? formatDate(report.timestamp) : 'Datetime'}
+              {report?.timestamp ? formatDate(report.timestamp) : "Datetime"}
             </Text>
           </View>
           <Image
             source={
               report?.reporterProfile
                 ? { uri: report.reporterProfile }
-                : require('@/assets/images/profile.png')
+                : require("@/assets/images/profile.png")
             }
             style={styles.reportImage}
           />
@@ -73,16 +101,18 @@ export default function ReportDetail() {
           <View style={styles.infoContainer}>
             <View style={styles.info}>
               <Text style={styles.infoHeaderText}>Date:</Text>
-              <Text style={styles.infoDesc}>{report?.date || 'N/A'}</Text>
+              <Text style={styles.infoDesc}>{formatNotificationTime(report?.createdAt) || "N/A"}</Text>
             </View>
             <View style={styles.info}>
               <Text style={styles.infoHeaderText}>Category:</Text>
-              <Text style={styles.infoDesc}>{report?.category || 'N/A'}</Text>
+              <Text style={styles.infoDesc}>{report?.category || "N/A"}</Text>
             </View>
             <View style={styles.info}>
               <Text style={styles.infoHeaderText}>Location:</Text>
               <Text style={styles.infoDesc}>
-                {`${report?.location.latitude || 'City'} ${report?.location.longitude || 'of Bacolod'}`}
+                {`${report?.location.latitude || "City"} ${
+                  report?.location.longitude || "of Bacolod"
+                }`}
               </Text>
             </View>
             <View style={styles.mapContainer}>
@@ -105,17 +135,26 @@ export default function ReportDetail() {
               </MapView>
             </View>
             <View style={styles.infoColumn}>
-              <Text style={[styles.infoHeaderText, styles.pad]}>Emergency Details</Text>
-              <Text style={styles.infoDesc}>{report?.details || 'No details available.'}</Text>
+              <Text style={[styles.infoHeaderText, styles.pad]}>
+                Emergency Details
+              </Text>
+              <Text style={styles.infoDesc}>
+                {report?.details || "No details available."}
+              </Text>
             </View>
             <View style={styles.infoColumn}>
               <Text style={[styles.infoHeaderText, styles.pad]}>Images</Text>
               <View style={styles.imageContainer}>
                 {report?.assets.map((item: any, idx: number) => (
-                  <Image
-                    source={item ? { uri: item.url } : require('@/assets/images/policeman.png')}
+                  <ImageModal
+                    source={
+                      item
+                        ? { uri: item.url }
+                        : require("@/assets/images/policeman.png")
+                    }
                     style={styles.image}
                     key={idx}
+                    resizeMode="cover" // Ensures the image scales correctly
                   />
                 ))}
               </View>
@@ -130,119 +169,119 @@ export default function ReportDetail() {
 const styles = ScaledSheet.create({
   container: {
     flex: 1,
-    position: 'relative',
-    overflow: 'scroll',
-    backgroundColor: '#faf9f6',
-    paddingBottom: '50@s',
+    position: "relative",
+    overflow: "scroll",
+    backgroundColor: "#faf9f6",
+    paddingBottom: "50@s",
     borderTopWidth: 2,
-    borderTopColor: '#dfdedd',
+    borderTopColor: "#dfdedd",
   },
   header: {
-    paddingHorizontal: '20@s',
-    paddingVertical: '15@vs',
-    backgroundColor: '#e6e6e6',
+    paddingHorizontal: "20@s",
+    paddingVertical: "15@vs",
+    backgroundColor: "#e6e6e6",
   },
   headerText: {
-    fontSize: '24@ms',
-    fontFamily: 'BeVietnamProBold',
+    fontSize: "24@ms",
+    fontFamily: "BeVietnamProBold",
   },
   reportsContainer: {
-    padding: '20@s',
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: '10@s',
-    paddingVertical: '20@vs',
-    paddingHorizontal: '20@s',
-    paddingRight: '60@s',
-    overflow: 'hidden',
+    padding: "20@s",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: "10@s",
+    paddingVertical: "20@vs",
+    paddingHorizontal: "20@s",
+    paddingRight: "60@s",
+    overflow: "hidden",
     borderBottomWidth: 1,
-    borderBottomColor: '#dfdedd',
+    borderBottomColor: "#dfdedd",
   },
   reportImage: {
-    resizeMode: 'cover',
-    width: '90@s',
-    height: '90@vs',
+    resizeMode: "cover",
+    width: "90@s",
+    height: "90@vs",
     borderRadius: 999,
     borderWidth: 1,
-    borderColor: '#343434',
+    borderColor: "#343434",
   },
   reportDesc: {
     flex: 1,
   },
   descTime: {
-    fontSize: '14@ms',
-    fontFamily: 'BeVietnamProRegular',
-    color: '#646b79',
+    fontSize: "14@ms",
+    fontFamily: "BeVietnamProRegular",
+    color: "#646b79",
   },
   descName: {
-    fontSize: '16@ms',
-    fontFamily: 'BeVietnamProSemiBold',
-    color: '#016ea6',
+    fontSize: "16@ms",
+    fontFamily: "BeVietnamProSemiBold",
+    color: "#016ea6",
   },
   descMessage: {
-    fontSize: '14@ms',
-    fontFamily: 'BeVietnamProMedium',
-    width: '95%',
-    color: '#b0adad',
+    fontSize: "14@ms",
+    fontFamily: "BeVietnamProMedium",
+    width: "95%",
+    color: "#b0adad",
   },
   information: {
     flex: 1,
-    padding: '24@s',
+    padding: "24@s",
   },
   infoText: {
-    fontSize: '24@ms',
-    fontFamily: 'BeVietnamProBold',
-    color: '#016ea6',
+    fontSize: "24@ms",
+    fontFamily: "BeVietnamProBold",
+    color: "#016ea6",
   },
   infoContainer: {
     flex: 1,
-    gap: '5@s',
-    paddingVertical: '8@vs',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    gap: "5@s",
+    paddingVertical: "8@vs",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
   },
   info: {
-    flexDirection: 'row',
-    gap: '5@s',
+    flexDirection: "row",
+    gap: "5@s",
   },
   infoColumn: {
-    width: '100%',
+    width: "100%",
   },
   infoHeaderText: {
-    fontSize: '16@ms',
-    fontFamily: 'BeVietnamProSemiBold',
-    color: '#b0adad',
+    fontSize: "16@ms",
+    fontFamily: "BeVietnamProSemiBold",
+    color: "#b0adad",
   },
   infoDesc: {
-    fontSize: '16@ms',
-    fontFamily: 'BeVietnamProMedium',
-    color: '#343434',
+    fontSize: "16@ms",
+    fontFamily: "BeVietnamProMedium",
+    color: "#343434",
   },
   pad: {
-    paddingTop: '25@vs',
+    paddingTop: "25@vs",
   },
   imageContainer: {
-    width: '100%',
-    height: 'auto',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    paddingVertical: '30@vs',
-    gap: '5@s',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    paddingBottom: '220@vs',
+    width: "100%",
+    height: "auto",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    paddingVertical: "30@vs",
+    gap: "5@s",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingBottom: "220@vs",
   },
   image: {
-    resizeMode: 'cover',
-    width: '90@s',
-    height: '90@s',
+    resizeMode: "cover",
+    width: "90@s",
+    height: "90@s",
   },
   mapContainer: {
-    width: '100%',
-    height: '30%',
+    width: "100%",
+    height: "30%",
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: "#000",
     borderRadius: 10,
-    marginTop: '10@vs',
+    marginTop: "10@vs",
   },
 });
